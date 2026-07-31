f1f1d0965d086de2b4c09a4edaa2f47dffd379ff
Abhijityaul <abhijityaul17@gmail.com>
2026-07-31 22:21:50 +0530

    add authentication for internal endpoint

--- backend/bike-service/src/main/java/com/bikerental/bike_service/security/SecurityConfig.java
+++ backend/bike-service/src/main/java/com/bikerental/bike_service/security/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/api/v1/bikes/public/**", "/bikes/v3/api-docs/**", "/api/v1/internal/**").permitAll()
                         .requestMatchers("/api/v1/bikes/partner/**", "/api/v1/bikes/storage/**").hasRole("PARTNER")
+                        .requestMatchers("/api/v1/bikes/internal/**").hasRole("CUSTOMER")
                         .requestMatchers("/api/v1/bikes/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
