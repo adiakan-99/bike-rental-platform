94719e3ce6daeb7b1650f5295f051c3b4fe5c543
Anupam Poddar <anupampoddar95@gmail.com>
2026-08-05 02:08:27 +0530

    Fix barrel exports after merge

--- frontend/bike-rental/src/features/admin/hooks/index.js
+++ frontend/bike-rental/src/features/admin/hooks/index.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 export { usePartnerAdmin } from "./usePartnerAdmin";
 export { usePendingBikes } from "./usePendingBikes";
-=======
-export { usePartnerAdmin } from "./usePartnerAdmin";
->>>>>>> origin/feature/frontend
--- frontend/bike-rental/src/features/dealer/hooks/index.js
+++ frontend/bike-rental/src/features/dealer/hooks/index.js
@@ -1,8 +1,4 @@
 export * from "./useDealerStats.js";
-<<<<<<< HEAD
 export * from "./useMyFleet.js";
 
 export { usePartnerService } from "./usePartnerService";
-=======
-export { usePartnerService } from "./usePartnerService";
->>>>>>> origin/feature/frontend
